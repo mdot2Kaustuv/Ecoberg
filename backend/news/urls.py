7f6040ff8699f3d1e9cccb50f742f8b1eb8ac17e
codeKGB <kbustuv@gmail.com>
2026-06-17 15:56:43 +0545

    Scraper

--- backend/news/urls.py
+++ backend/news/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
    path('', views.NewsList, name='news-list'),
+   path('scraper/', views.scraper, name='news-scraper')
 ]
--- backend/news/views.py
+++ backend/news/views.py
@@ -1,6 +1,8 @@
 import requests
 from decouple import config
 from django.http import JsonResponse
+import pandas as pd
+import pycountry
 
 def NewsList(request):
     access_key = config('YOUR_ACCESS_KEY')
@@ -21,9 +23,6 @@ def NewsList(request):
     
 
     res = r.json()
-    print("debug: ", r.status_code)
-    print(res)
-
     data = res['data']
     author = []
     title = []
@@ -50,3 +49,57 @@ def NewsList(request):
     news = zip(title, description, image, url, author, source, date)
 
     return JsonResponse({'news': news_list})
+
+
+def scraper(JsonResponse) :
+
+    url = "https://www.worldometers.info/greenhouse-gas-emissions/greenhouse-gas-emissions-by-country/"
+
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+    response = requests.get(url, headers=headers)
+
+    tables = pd.read_html(response.text)
+
+    print(f"Found {len(tables)} tables")
+    df = tables[0]
+
+    df.columns = ['index', 'country', 'total_emissions', 'one_year_change', 'per_capita', 'share']
+
+
+    df['total_emissions'] = pd.to_numeric(df['total_emissions'].astype(str).str.replace(',', ''), errors='coerce')
+    df['per_capita'] = pd.to_numeric(df['per_capita'].astype(str).str.replace(',', ''), errors='coerce')
+   
+   
+   
+    def get_country_code(country_name) :
+        try:
+                
+            return pycountry.countries.lookup(country_name).alpha_3
+        except LookupError:
+                mapping = {
+                    "United States": "USA",
+                    "United Kingdom": "GBR",
+                    "Russia": "RUS",
+                    "South Korea": "KOR",
+                    "Iran": "IRN",
+                    "Vietnam": "VNM",
+                    "Syria": "SYR",
+                    "Venezuela": "VEN"
+                }
+                return mapping.get(country_name, None)
+        
+    df['code'] = df['country'].apply(get_country_code)
+
+
+    clean_records = []
+    for _, row in df.iterrows():
+            clean_records.append({
+                "code": row['code'],
+                "country": row['country'],
+                "total": int(row['total_emissions']),
+                "per_capita": float(row['per_capita'])
+            })
+
+    return JsonResponse(clean_records, safe=False)

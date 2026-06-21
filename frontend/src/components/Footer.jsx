7d1156486c5da57512e19b8667431c17d47decc5
codeKGB <kbustuv@gmail.com>
2026-06-21 14:18:51 +0545

    Sources

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,14 +4,7 @@ import { Leaf} from 'lucide-react';
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
-
-
-  
-  
-  
-  
-  
-  return (
+return (
     <footer className="bg-emerald-900 text-emerald-300 py-8">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-12">
         <div className="flex flex-col md:flex-row justify-between items-center">

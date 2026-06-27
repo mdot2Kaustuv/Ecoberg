25438b2bb6424995f5b3aef651a0ccafad473288
codeKGB <kbustuv@gmail.com>
2026-06-27 12:57:32 +0545

    Fixed bot backend

--- backend/chatbot/settings.py
+++ backend/chatbot/settings.py
@@ -49,6 +49,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
 ROOT_URLCONF = 'chatbot.urls'
 
 TEMPLATES = [
--- backend/chatbot/views.py
+++ backend/chatbot/views.py
@@ -2,10 +2,12 @@ import requests
 import json
 from django.http import JsonResponse
 from decouple import config
+from django.views.decorators.csrf import csrf_exempt
 
 
 
 
+@csrf_exempt 
 def get_chatbot_response(request):
     if request.method == 'POST':
         data = json.loads(request.body)
@@ -24,7 +26,7 @@ def get_chatbot_response(request):
                     "content": user_message
                 }
             ],
-            "model": "grok-4-1-fast",
+            "model": "llama-3.3-70b-versatile",
             "stream": False,
             "temperature": 1
         })
@@ -34,7 +36,10 @@ def get_chatbot_response(request):
         }
 
         response = requests.request("POST", url, headers=headers, data=payload)
+        print("Status: ",response.status_code)
+        print("Body:", response.text)  
 
+                
         if response.status_code == 200:
             response_data = response.json()
             chatbot_reply = response_data['choices'][0]['message']['content']
